071ef52d1708d4d926adee144fd5e7bfe1e21ad1
João Ferreira <joaogferreira@ua.pt>
2020-03-29 19:54:44 +0100

    html

---
+++ hw1-tqs/src/main/java/com/example/hw1tqs/Hw1TqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw1tqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw1TqsApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw1TqsApplication.class, args);
+	}
+
+}
