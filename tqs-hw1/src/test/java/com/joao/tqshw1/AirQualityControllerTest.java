478902d90be9400929d9756571acf42e06fac9c5
joaogferreira <47529390+joaogferreira@users.noreply.github.com>
2020-04-09 19:43:01 +0100

    Update AirQualityControllerTest.java

--- tqs-hw1/src/test/java/com/joao/tqshw1/AirQualityControllerTest.java
+++ tqs-hw1/src/test/java/com/joao/tqshw1/AirQualityControllerTest.java
@@ -1,33 +1,27 @@
 package com.joao.tqshw1;
-
-import org.junit.Before;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-import org.junit.Test;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import java.util.ArrayList;
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@EnableWebMvc
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class AirQualityControllerTest {
     ArrayList<String> cities_available = new ArrayList<>();
-
-    private MockMvc mockMvc;
-
     @Autowired
-    AirQualityController controller = new AirQualityController();
-
-    @Before
-    public void setup() throws Exception { this.mockMvc = standaloneSetup(this.controller).build(); }
-
+    private MockMvc mockMvc;
+    @Autowired
+    AirQualityController controller; //= new AirQualityController();
+    @BeforeEach
+    public void setup() throws Exception {
+        //this.mockMvc = standaloneSetup(this.controller).build();
+    }
     @Test
     public void testAir() throws Exception {
         //Add all the cities available
@@ -35,21 +29,19 @@ public class AirQualityControllerTest {
         cities_available.add("lisbon");cities_available.add("berlin");cities_available.add("tokyo");
         cities_available.add("munchen");cities_available.add("denver");cities_available.add("helsinki");
         cities_available.add("stockholm");cities_available.add("moscow");cities_available.add("madrid");
-
-
         for (int i=0;i<cities_available.size();i++){
             mockMvc.perform(MockMvcRequestBuilders.get("/api/air/"+cities_available.get(i)))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.status").value("ok"));
         }
     }
-
     @Test
     public void testStations() throws Exception {
-        //Está a retornar vazio
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/stations"))
+        //Estava a retornar vazio
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/stations"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.0").value("{\"city\":\"Shanghai\",\"id\":0}"));
-
+                .andReturn();;
+        String json = result.getResponse().getContentAsString();
+        System.out.println( "json is: " + json);
     }
 }
