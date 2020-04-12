3b71c12e1a9660451e4f57eba6b40cd034e3e5b5
João Ferreira <joaogferreira@ua.pt>
2020-04-12 04:33:04 +0100

    smth

--- tqs-hw1/src/test/java/com/joao/tqshw1/AirQualityControllerTest.java
+++ tqs-hw1/src/test/java/com/joao/tqshw1/AirQualityControllerTest.java
@@ -6,15 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.swing.tree.ExpandVetoException;
 import java.util.ArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
-
-
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
